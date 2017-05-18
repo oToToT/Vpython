2ee955f86c8373011593a9dd5c835b85b1e7b44e
Tommy <Tommy@Tommy.local>
2017-05-18 13:00:13 +0800

    modify method

--- work.py
+++ work.py
@@ -3,11 +3,13 @@ Box = box(pos=vector(0,0,0), length=10, height=0.1, width=5, color=(1,1,1))
 Light=sphere(pos=vector(2,3,0), radius=0.05, color=(1,1,0), make_trail=True)
 Target_pos=vector(-3,0,0)
 Theta = acos(dot(Light.pos-Target_pos, Box.axis-Target_pos)/abs(Light.pos-Target_pos)/abs(Box.axis-Target_pos))
+re=1
 dt=0.01
 t=0
 while True:
 	rate(1/dt)
 	Light.pos-=dt*vector(cos(Theta), sin(Theta), 0)
-	if abs(Light.pos-Target_pos)<=0.005:
+	if abs(Light.pos-Target_pos)<=0.1 and re:
+		re=0
 		Theta=-Theta
 	t+=dt
