c761eae579f1de5464d52eccfe4a3ba97a1a90e7
Tommy <ty1208chiang@gmail.com>
2017-06-21 23:51:19 +0800

    half done dynamic array

--- darray.py
+++ darray.py
@@ -1,7 +1,18 @@
-from numpy import array as arr
+import numpy as np
 
 class dArray(object):
-	"""docstring for dArray"""
-	def __init__(self, arg):
+	def __init__(self, tp=float):
 		super(dArray, self).__init__()
-		self.arg = arg
+		self.arr = np.array(np.zeros(1, dtype=tp))
+		self.size = 1
+		self.front = 0
+		self.type = tp
+	def append(self, x):
+		if self.front + 1 >= self.size:
+			self.arr = np.append(self.arr, np.zeros(self.size, dtype=self.type))
+			self.size *= 2
+		self.arr[self.front] = x
+		self.front += 1
+	def back(self):
+		return self.arr[self.front-1]
+
